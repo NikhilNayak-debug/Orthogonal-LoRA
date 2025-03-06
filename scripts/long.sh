dbdd7a4da0dd36cfbc395aaa011faee3115bc39f
NikhilNayak-debug <nikhilshivakn.cs17@rvce.edu.in>
2025-03-06 19:20:43 +0000

    removing transformers cache from running script

--- scripts/long.sh
+++ scripts/long.sh
@@ -2,7 +2,7 @@
 set -x
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export TRANSFORMERS_CACHE=/root/.cache/huggingface
+# export TRANSFORMERS_CACHE=/root/.cache/huggingface
 
 port=$(shuf -i25000-30000 -n1)
  
